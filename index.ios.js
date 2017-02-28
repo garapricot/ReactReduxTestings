7d38dc97f8880edbe27cc752cc1311a6dae63ffb
gareginhambardzumyan <yerevanrockcity@gmail.com>
2017-02-28 18:37:31 +0400

    initial commit

--- index.ios.js
+++ index.ios.js
@@ -3,42 +3,38 @@ import {Provider} from 'react-redux';
 import{createStore,applyMiddleware,combineReducers,compose} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import createLogger from 'redux-logger'
+import reducer from './app/reducers'
+// middleware that logs actions
+const loggerMiddleware = createLogger({ predicate: (getState, action) => __DEV__  });
+function configureStore(initialState) {
+  const enhancer = compose(
+    applyMiddleware(
+      thunkMiddleware, // lets us dispatch() functions
+      loggerMiddleware,
+    ),
+  );
+  return createStore(reducer, initialState, enhancer);
+}
+    const store = configureStore({});
 import {
   AppRegistry,
-  StyleSheet,
   Text,
   View
 } from 'react-native';
-export default class ReactReduxTestings extends Component {
+  class ReactReduxTestings extends Component {
   render() {
-    console.log("Hello React Redux!!!");
     return (
-      <View style={styles.container}>
-        <Text style={styles.instructions}>
-          React Redux implementation
+      <View>
+        <Text>
+           React Redux implementation
         </Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('ReactReduxTestings', () => ReactReduxTestings);
+  const App = () => (
+  <Provider store={store}>
+    <ReactReduxTestings />
+  </Provider>
+)
+AppRegistry.registerComponent('ReactReduxTestings', () => App);
