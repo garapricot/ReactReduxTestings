bfbd5f8d959b4f1dc5737a07e405464fef4b0aa1
gareginhambardzumyan <yerevanrockcity@gmail.com>
2017-02-28 18:16:45 +0400

    * mistake

--- index.ios.js
+++ index.ios.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Provider} from 'react-redux';
-import{createStore,applyMiddleware,combineReduxers,compose} from 'redux'
+import{createStore,applyMiddleware,combineReducers,compose} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import createLogger from 'redux-logger'
 import {
